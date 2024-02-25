526896a982ad00f6d55b22869f3447ed1cc8946a
dev+1-at-029958659886 <miguelofoliveir>
2024-02-25 18:11:49 -0300

    atualizando header e informacoes do motorista

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -13,32 +13,7 @@ const Guide = () => {
         <div className="lg:pt-12">
           <div>
             <div className="flex">
-              <div className="lg:w-1/2">
-                <h2 className="bold-20 lg:bold-32">SOBRE O MOTORISTA</h2>
-                <p className="regular-16 text-gray-30 pt-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
-                  Pellentesque habitant morbi tristique senectus et netus et
-                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
-                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
-                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
-                  ullamcorper malesuada. In venenatis sapien lectus, id
-                  porttitor urna pharetra eu. Integer placerat a justo vitae
-                  mattis.
-                </p>
-                <p className="regular-16 text-gray-30 pt-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
-                  Pellentesque habitant morbi tristique senectus et netus et
-                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
-                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
-                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
-                  ullamcorper malesuada. In venenatis sapien lectus, id
-                  porttitor urna pharetra eu. Integer placerat a justo vitae
-                  mattis.
-                </p>
-              </div>
-              <div className="lg:px-4 lg:w-1/2">
+              <div className="lg:pr-8 lg:w-1/2">
                 <Image
                   src="/motorista-dog.jpg"
                   alt="Motorista e o pet"
@@ -47,10 +22,83 @@ const Guide = () => {
                   height={29}
                 />
               </div>
+              <div className="lg:w-1/2">
+                <h2 className="bold-20 lg:bold-32">
+                  CONFIANÇA, GENTILEZA, PONTUALIDADE, CUIDADO E AMOR PELOS PETS.
+                </h2>
+                <p className="regular-16 text-gray-30 pt-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
+                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
+                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
+                  ullamcorper malesuada. In venenatis sapien lectus, id
+                  porttitor urna pharetra eu. Integer placerat a justo vitae
+                  mattis.
+                </p>
+                <p className="regular-16 text-gray-30 pt-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
+                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
+                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
+                  ullamcorper malesuada. In venenatis sapien lectus, id
+                  porttitor urna pharetra eu. Integer placerat a justo vitae
+                  mattis.
+                </p>
+                <p className="regular-16 text-gray-30 pt-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
+                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
+                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
+                  ullamcorper malesuada. In venenatis sapien lectus, id
+                  porttitor urna pharetra eu. Integer placerat a justo vitae
+                  mattis.
+                </p>
+              </div>
             </div>
           </div>
           <div>
-            <div className="flex lg:pt-20">
+            <div className="flex">
+              <div className="lg:w-1/2">
+                <p className="regular-16 text-gray-30 pt-8">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
+                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
+                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
+                  ullamcorper malesuada. In venenatis sapien lectus, id
+                  porttitor urna pharetra eu. Integer placerat a justo vitae
+                  mattis.
+                </p>
+                <p className="regular-16 text-gray-30 pt-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
+                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
+                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
+                  ullamcorper malesuada. In venenatis sapien lectus, id
+                  porttitor urna pharetra eu. Integer placerat a justo vitae
+                  mattis.
+                </p>
+                <p className="regular-16 text-gray-30 pt-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
+                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
+                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
+                  ullamcorper malesuada. In venenatis sapien lectus, id
+                  porttitor urna pharetra eu. Integer placerat a justo vitae
+                  mattis.
+                </p>
+              </div>
               <div className="lg:px-4 lg:w-1/2">
                 <Image
                   src="/pets-dog-cat.jpg"
@@ -59,31 +107,6 @@ const Guide = () => {
                   width={300}
                   height={29}
                 />
-              </div>
-              <div className="lg:w-1/2">
-                <h2 className="bold-20 lg:bold-32">SOBRE O MOTORISTA</h2>
-                <p className="regular-16 text-gray-30 pt-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
-                  Pellentesque habitant morbi tristique senectus et netus et
-                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
-                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
-                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
-                  ullamcorper malesuada. In venenatis sapien lectus, id
-                  porttitor urna pharetra eu. Integer placerat a justo vitae
-                  mattis.
-                </p>
-                <p className="regular-16 text-gray-30 pt-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                  ac blandit odio. Nulla vel cursus lectus, non accumsan dolor.
-                  Pellentesque habitant morbi tristique senectus et netus et
-                  malesuada fames ac turpis egestas. Phasellus pulvinar ligula
-                  ac blandit fringilla. Ut egestas sem est, vitae dictum nisl
-                  cursus in. Proin id semper odio. Aliquam eleifend erat in odio
-                  ullamcorper malesuada. In venenatis sapien lectus, id
-                  porttitor urna pharetra eu. Integer placerat a justo vitae
-                  mattis.
-                </p>
               </div>
             </div>
           </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,57 @@
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-import Link from "next/link"
-import Button from "./Button"
+import { NAV_LINKS, SOCIALS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
     <nav className="max-container padding-container relative z-30 py-5">
-      <div className="flex justify-center pb-4 border-b-2">TRANSPORTE DE PET EM SÃO PAULO</div>
-     <div className="flex justify-center pt-4">
-     <Link href="/">
-        <Image src="/logo-pet-trim.png" alt="logo motorista de pet" width={84} height={39} />
-      </Link>
-     </div>
-     <div className="flex justify-center items-center lg:mt-8">
-     <ul className="hidden h-full gap-12 lg:flex">
-        {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
-            {link.label}
-          </Link>
-        ))}
-      </ul>
-      <div className="lg:flexCenter lg:ml-8 hidden">
-        <Button
-          type="button"
-          title="Agendar Corrida"
-          variant="btn_green_light"
-        />
+      <div className="flex justify-center pb-4 border-b-2">
+        TRANSPORTE DE PET EM SÃO PAULO
       </div>
-     </div>
-      <Image 
+      <div className="flex justify-center pt-4">
+        <div className="w-4/12"></div>
+        <div className="w-4/12 flex justify-center">
+          <Link href="/">
+            <Image
+              src="/logo-pet-trim.png"
+              alt="logo motorista de pet"
+              width={84}
+              height={39}
+            />
+          </Link>
+        </div>
+        <div className="w-4/12 flex justify-end items-center">
+          <ul className="regular-14 flex gap-4 text-gray-30">
+            {SOCIALS.links.map((link) => (
+              <Link className="mt-2" href={link.link} key={link.icon}>
+                <Image src={link.icon} alt="logo" width={24} height={24} />
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="flex justify-center items-center lg:mt-8">
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+        <div className="lg:flexCenter lg:ml-8 hidden">
+          <Button
+            type="button"
+            title="Agendar Corrida"
+            variant="btn_green_light"
+          />
+        </div>
+      </div>
+      <Image
         src="menu.svg"
         alt="menu"
         width={32}
@@ -36,7 +59,7 @@ const Navbar = () => {
         className="inline-block cursor-pointer lg:hidden"
       />
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
