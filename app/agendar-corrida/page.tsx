a6eacc6b25f250651c09064ade5753e2ea30ad16
dev+1-at-029958659886 <miguelofoliveir>
2024-03-21 23:20:47 -0300

    formulario agendar corrida com validacao completa

--- app/agendar-corrida/page.tsx
+++ app/agendar-corrida/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import ScrollAnimation from "@/components/ScrollAnimation";
 import { NUMBER_LINK_WHATSAPP } from "@/constants";
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
+import * as yup from "yup";
+import { AnySchema, ValidationError } from "yup";
 
 interface FormData {
   nome: string;
@@ -11,7 +12,29 @@ interface FormData {
   quantidadePets: string;
 }
 
+interface FormErrors {
+  [key: string]: string;
+}
+
+const validationSchema = yup.object().shape({
+  nome: yup.string().required("O campo Nome Completo é obrigatório."),
+  dataHora: yup.date().required("O campo Data e Hora é obrigatório."),
+  enderecoPartida: yup
+    .string()
+    .required("O campo Endereço de Partida é obrigatório."),
+  enderecoDestino: yup
+    .string()
+    .required("O campo Endereço de Destino é obrigatório."),
+  quantidadePets: yup
+    .number()
+    .required("O campo Quantidade de Pets é obrigatório.")
+    .positive("O número deve ser positivo.")
+    .integer("O número deve ser inteiro."),
+});
+
 export default function Contato() {
+  const [errors, setErrors] = useState<FormErrors>({});
+
   const [formData, setFormData] = useState<FormData>({
     nome: "",
     dataHora: "",
@@ -20,17 +43,62 @@ export default function Contato() {
     quantidadePets: "",
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await validationSchema.validate(formData, { abortEarly: false });
+      setErrors({});
+      enviarParaWhatsApp(formData);
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        const validationErrors: FormErrors = {};
+        error.inner.forEach((err) => {
+          if (err.path) validationErrors[err.path] = err.message;
+        });
+        setErrors(validationErrors);
+      }
+    }
+  };
+
+  const handleChange = async (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
-    setFormData((prevState) => ({
+
+    setFormData((prevState: FormData) => ({
       ...prevState,
       [name]: value,
     }));
-  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    enviarParaWhatsApp(formData);
+    try {
+      // Usando asserção de tipo para AnySchema
+      const fieldSchema: AnySchema = yup.reach(
+        validationSchema,
+        name
+      ) as AnySchema;
+
+      await fieldSchema.validate(value);
+
+      setErrors((prevErrors: FormErrors) => {
+        const updatedErrors = { ...prevErrors };
+        delete updatedErrors[name];
+        return updatedErrors;
+      });
+    } catch (error: unknown) {
+      if (error instanceof ValidationError) {
+        const validationError = error as any; // Assegurando explicitamente o tipo como `any`.
+
+        setErrors((prevErrors: FormErrors) => ({
+          ...prevErrors,
+          [validationError.path ? validationError.path : "unknown"]:
+            validationError.message,
+        }));
+      } else {
+        // Tratamento para outros tipos de erros, se necessário
+        console.error("An unexpected error occurred:", error);
+      }
+    }
   };
 
   function enviarParaWhatsApp(formData: FormData) {
@@ -62,20 +130,9 @@ export default function Contato() {
     window.open(whatsappUrl, "_blank");
   }
 
-  const leftVariants = {
-    hidden: { x: -100, opacity: 0 },
-    visible: { x: 0, opacity: 1, transition: { duration: 1 } },
-  };
-
-  const rightVariants = {
-    hidden: { x: 100, opacity: 0 },
-    visible: { x: 0, opacity: 1, transition: { duration: 1 } },
-  };
-
   return (
     <section>
       <div className="lg:px-8 px-4 py-4 lg:pb-32 pb-10 lg:py-20 green-two-bg">
-        <ScrollAnimation variants={leftVariants}>
           <div className="lg:flex pt-4">
             <div className="lg:w-1/2 w-full lg:mr-12 green-main-bg rounded-lg p-8">
               <h2 className="bold-24 lg:bold-40 cinzel">
@@ -92,8 +149,15 @@ export default function Contato() {
                     id="nome"
                     value={formData.nome}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus-visible:border-teal-500 focus-visible:ring focus-visible:ring-teal-500 focus-visible:ring-opacity-50"
+                    className={`w-full p-2 border rounded-md shadow-sm focus:outline-none ${
+                      errors.nome
+                        ? "border-red-500"
+                        : "border-gray-300 focus-visible:border-teal-500"
+                    }`}
                   />
+                  {errors.nome && (
+                    <p className="text-red-500 text-sm mt-1">{errors.nome}</p>
+                  )}
                 </div>
                 <div className="flex flex-col">
                   <label htmlFor="dataHora" className="mb-2">
@@ -105,8 +169,17 @@ export default function Contato() {
                     id="dataHora"
                     value={formData.dataHora}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus-visible:border-teal-500 focus-visible:ring focus-visible:ring-teal-500 focus-visible:ring-opacity-50"
+                    className={`w-full p-2 border rounded-md shadow-sm focus:outline-none ${
+                      errors.dataHora
+                        ? "border-red-500"
+                        : "border-gray-300 focus-visible:border-teal-500"
+                    }`}
                   />
+                  {errors.dataHora && (
+                    <p className="text-red-500 text-sm mt-1">
+                     O campo Data e Hora é obrigatório.
+                    </p>
+                  )}
                 </div>
                 <div className="flex flex-col">
                   <label htmlFor="enderecoPartida" className="mb-2">
@@ -118,8 +191,17 @@ export default function Contato() {
                     id="enderecoPartida"
                     value={formData.enderecoPartida}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus-visible:border-teal-500 focus-visible:ring focus-visible:ring-teal-500 focus-visible:ring-opacity-50"
+                    className={`w-full p-2 border rounded-md shadow-sm focus:outline-none ${
+                      errors.enderecoPartida
+                        ? "border-red-500"
+                        : "border-gray-300 focus-visible:border-teal-500"
+                    }`}
                   />
+                  {errors.enderecoPartida && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.enderecoPartida}
+                    </p>
+                  )}
                 </div>
                 <div className="flex flex-col">
                   <label htmlFor="enderecoDestino" className="mb-2">
@@ -131,8 +213,17 @@ export default function Contato() {
                     id="enderecoDestino"
                     value={formData.enderecoDestino}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus-visible:border-teal-500 focus-visible:ring focus-visible:ring-teal-500 focus-visible:ring-opacity-50"
+                    className={`w-full p-2 border rounded-md shadow-sm focus:outline-none ${
+                      errors.enderecoDestino
+                        ? "border-red-500"
+                        : "border-gray-300 focus-visible:border-teal-500"
+                    }`}
                   />
+                  {errors.enderecoDestino && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.enderecoDestino}
+                    </p>
+                  )}
                 </div>
                 <div className="flex flex-col">
                   <label htmlFor="quantidadePets" className="mb-2">
@@ -144,8 +235,17 @@ export default function Contato() {
                     id="quantidadePets"
                     value={formData.quantidadePets}
                     onChange={handleChange}
-                    className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus-visible:border-teal-500 focus-visible:ring focus-visible:ring-teal-500 focus-visible:ring-opacity-50"
+                    className={`w-full p-2 border rounded-md shadow-sm focus:outline-none ${
+                      errors.quantidadePets
+                        ? "border-red-500"
+                        : "border-gray-300 focus-visible:border-teal-500"
+                    }`}
                   />
+                  {errors.quantidadePets && (
+                    <p className="text-red-500 text-sm mt-1">
+                      O campo Quantidade de Pets é obrigatório.
+                    </p>
+                  )}
                 </div>
                 <button
                   type="submit"
@@ -156,7 +256,6 @@ export default function Contato() {
               </form>
             </div>
           </div>
-        </ScrollAnimation>
       </div>
     </section>
   );
