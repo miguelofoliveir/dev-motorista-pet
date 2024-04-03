312f9c8718ff1193a4916ed7789a7b9b60837755
dev+1-at-029958659886 <miguelofoliveir>
2024-04-03 19:29:58 -0300

    atualizando readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# dev-motorista-pet
+
+# Motorista de Pet
+
+## Como Rodar o Projeto
+
+Este projeto é desenvolvido com React e Next.js. Para rodá-lo localmente, siga os passos abaixo:
+
+1. Clone o repositório
+
+2. Entre no diretório do projeto:
+cd motorista_de_pet
+
+3. Instale as dependências:
+npm install
+
+4. Execute o projeto em modo de desenvolvimento:
+npm run dev
+
+Agora o projeto deve estar acessível em http://localhost:3000
+
+## Como Contribuir
+
+Contribuições para melhorar nosso serviço e atendimento são sempre bem-vindas. Se você tem uma ideia ou sugestão, por favor, entre em contato através de um dos nossos canais de comunicação.
+
+## Licença
+
+Este projeto é licenciado sob a Licença MIT - veja o arquivo LICENSE.md para detalhes.
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -7,8 +6,17 @@ import Head from "next/head";
 import CookieConsentModal from "@/components/CookieConsentModal";
 
 export const metadata: Metadata = {
-  title: "Motorista de Pet",
-  description: "Transporte de Pets",
+  title: "Motorista de Pet - Transporte de Pets",
+  description:
+    "Motorista de Pet oferece um serviço de transporte de pets com cuidado, responsabilidade e amor. Conheça nosso trabalho e sinta-se seguro em deixar seu pet em boas mãos.",
+  openGraph: {
+    type: "website",
+    images: "https://motoristadepet.com.br/logo-fundo.png",
+    url: "https://motoristadepet.com.br",
+    title: "Motorista de Pet - Transporte de Pets",
+    description:
+      "Motorista de Pet oferece um serviço de transporte de pets com cuidado, responsabilidade e amor. Conheça nosso trabalho e sinta-se seguro em deixar seu pet em boas mãos.",
+  },
 };
 
 export default function RootLayout({
@@ -20,43 +28,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Head>
-          <title>Motorista de Pet - Transporte de Pets</title>
-          <meta
-            name="description"
-            content="Motorista de Pet oferece um serviço de transporte de pets com cuidado, responsabilidade e amor. Conheça nosso trabalho e sinta-se seguro em deixar seu pet em boas mãos."
-          />
           <meta charSet="utf-8" />
           <meta
             name="viewport"
             content="initial-scale=1.0, width=device-width"
-          />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://motoristadepet.com.br/" />
-          <meta
-            property="og:title"
-            content="Motorista de Pet - Transporte de Pets"
-          />
-          <meta
-            property="og:description"
-            content="Oferecemos um serviço totalmente humanizado para o transporte do seu pet. Conheça a história de Renato e como o Motorista de Pet se tornou uma referência em cuidado e amor pelos animais."
-          />
-          <meta
-            property="og:image"
-            content="https://motoristadepet.com.br/logo-pet-trim.png"
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:url" content="https://motoristadepet.com.br/" />
-          <meta
-            name="twitter:title"
-            content="Motorista de Pet - Transporte de Pets"
-          />
-          <meta
-            name="twitter:description"
-            content="Descubra o serviço de transporte de pets que reflete amor e cuidado em cada viagem. Conheça nossa equipe dedicada e apaixonada pelo que faz."
-          />
-          <meta
-            name="twitter:image"
-            content="https://motoristadepet.com.br/logo-pet-trim.png"
           />
         </Head>
         <Navbar />
