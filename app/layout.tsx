f1dc81de1da0c8040b6bd8839fdf4f4d815ce8b2
dev+1-at-029958659886 <miguelofoliveir>
2025-01-29 20:11:41 -0300

    feat: added new GTM and GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,25 +34,80 @@ export default function RootLayout({
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
-        </Head>
-      
-         {/* Google Tag Manager - gtag.js */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-EHYGYYE8XQ"
-        strategy="afterInteractive"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-      >
-        {`
+
+          {/* Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-EHYGYYE8XQ"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EHYGYYE8XQ');
+          `}
+          </Script>
+
+          {/* Google Ads Conversion Tracking */}
+          <Script id="google-ads" strategy="afterInteractive">
+            {`
+            gtag('config', 'AW-16561576472');
+          `}
+          </Script>
+
+          {/* Google Ads Conversion Event on Button */}
+          <Script id="google-conversion-event" strategy="afterInteractive">
+            {`
+            function gtag_report_conversion(url) {
+              var callback = function () {
+                if (typeof(url) != 'undefined') {
+                  window.location = url;
+                }
+              };
+              gtag('event', 'conversion', {
+                  'send_to': 'AW-16561576472/tVPNCJfM0JYaEJi0ltk9',
+                  'event_callback': callback
+              });
+              return false;
+            }
+          `}
+          </Script>
+          {/* Google Tag Manager - gtag.js */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-EHYGYYE8XQ"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-EHYGYYE8XQ');
         `}
-      </Script>
-       
+          </Script>
+          {/* Google Tag Manager */}
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NK8SLFC4');
+          `}
+          </Script>
+        </Head>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NK8SLFC4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Navbar />
         <main className="relative overflow-hidden">{children}</main>
         <Footer />
